50bfe60bee3136b2dbad5e1a6d16613f7744d78d
Dev-El-badry <miliarder96@gmail.com>
2020-04-12 01:44:41 +0200

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateConfigModule } from './translate-config.module';
+
+//services
+import { TranslateConfigService } from './shared/translate-config.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    TranslateConfigModule
   ],
-  providers: [],
+  providers: [TranslateConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
